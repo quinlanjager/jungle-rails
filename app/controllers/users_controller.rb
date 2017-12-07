732484c371845c56cd5a7282cb446f4957fa8861
Quinlan Jager <quinlan.jager@gmail.com>
2017-12-07 23:34:11 +0000

    Added proper login button name.

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -10,7 +10,7 @@ class UsersController < ApplicationController
   # post route for logging in
   def create_session
     @user = find_user
-    if @user == false
+    if @user == nil or @user == false
       @user = User.new
       @user.errors.add(:base, "E-mail or password did not match our records")
       render :login
@@ -20,14 +20,17 @@ class UsersController < ApplicationController
     redirect_to "/"
   end
 
+  # post route for making a new user
   def create
     @user = create_user
 
+    # Check if all inputs are correct
     if @user.valid? == false
       render :show
       return
     end
 
+    # if a user exists
     if User.find_by(email: @user.email)
       @user.errors.add :base, "Email is already in use."
       render :show
@@ -39,13 +42,13 @@ class UsersController < ApplicationController
     redirect_to "/"
   end
 
-
   def logout
     session[:user_id] = nil
     redirect_to "/"
   end
 
   private
+    # create and return a new user
     def create_user
       user_params = params.require(:user).permit(:first_name, :last_name, :email, :password)
       user = User.new first_name: user_params[:first_name], last_name: user_params[:last_name], email: user_params[:email], password: ''
@@ -53,6 +56,7 @@ class UsersController < ApplicationController
       user
     end
 
+    # find a user, for logging in.
     def find_user
       user_params = params.require(:user).permit(:email, :password)
       User.find_by(email: user_params[:email]).try(:authenticate, user_params[:password])
