240ee98f453d91714049dc0e389ce9dd1130620d
Quinlan Jager <quinlan.jager@gmail.com>
2017-12-08 20:10:37 +0000

    Can now post reviews.

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -1,5 +1,10 @@
 class ReviewsController < ApplicationController
   def create
+    @review = Review.new review_params
+    @review.user_id = @user || nil
+    @review.product_id = params[:product_id]
+    @review.save
+    redirect_to Product.find(@review.product_id), notice: "Review posted succesfully!"
   end
 
   def edit
@@ -7,4 +12,9 @@ class ReviewsController < ApplicationController
 
   def destroy
   end
+
+  private
+    def review_params
+      params.require(:review).permit(:rating, :description)
+    end
 end
