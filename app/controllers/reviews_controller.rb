8e36eee2620d8eff494c5b26c71f1d83d7cc0e84
Quinlan Jager <quinlan.jager@gmail.com>
2017-12-08 20:32:25 +0000

    Added styling and fixed user_id tracking bug

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -1,8 +1,17 @@
 class ReviewsController < ApplicationController
   def create
+    product_id = params[:product_id]
+
     @review = Review.new review_params
-    @review.user_id = @user || nil
-    @review.product_id = params[:product_id]
+    @review.user_id = @user.id || nil
+    @review.product_id = product_id
+
+    # check if valid before saving
+    if not @review.valid?
+      redirect_to Product.find(product_id), alert: @review.errors.full_messages[0]
+      return
+    end
+
     @review.save
     redirect_to Product.find(@review.product_id), notice: "Review posted succesfully!"
   end
