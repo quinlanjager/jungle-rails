cdd8c8cd6172d71fdea72b3d51aac30eb571ad45
Quinlan Jager <quinlan.jager@gmail.com>
2017-12-12 20:41:48 +0000

    Testing home page and product page.

---
+++ spec/features/product_details_spec.rb
@@ -0,0 +1,25 @@
+require 'rails_helper'
+
+RSpec.feature "ProductDetails", type: :feature, js: true do
+  before :each do
+    @category = Category.create! name: 'Apparel'
+    10.times do |n|
+      @category.products.create!(
+        name:  Faker::Hipster.sentence(3),
+        description: Faker::Hipster.paragraph(4),
+        image: open_asset('apparel1.jpg'),
+        quantity: 10,
+        price: 64.99
+      )
+    end
+  end
+
+  scenario "They can navigate to the product details page from the home page and see the description" do
+    visit root_path
+    page.first('.actions').find_link('Details »').click
+    sleep 2
+    url = URI.parse(current_url).to_s
+    expect(page).to have_text(Product.find(url.split('/').last).description)
+    save_screenshot "description.png"
+  end
+end
